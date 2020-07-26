04a20f28c52a62860de0aeced4a1025bb1cb0b09
fco-junior <cassemirojunior@alu.ufc.br>
2020-07-25 22:24:49 -0300

    atividade 1

--- atividade_1/src/App.js
+++ atividade_1/src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import './App.css';
-import Questao1 from './components/Questao1';
-import Questao2 from './components/Questao2';
-import Questao3 from './components/Questao3';
-import Questao4 from './components/Questao4';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Questao1 from './components/Questao1/Questao1';
+import Questao2 from './components/Questao2/Questao2';
+import Questao3 from './components/Questao3/Questao3';
+import Questao4 from './components/Questao4/Questao4';
 
 function App() {
   return (
     <div className="App">
-      <Questao4 nome='Francisco Costa Cassemiro Júnior'
-                curso='Sistemas de Informação'
-                cidade='Capistrano - CE'>
-        <Questao2/>
-      </Questao4>
+      <div className="card" Style="width: 30rem;">
+          <div className="card-header" id="headingOne">
+            <h5 className="mb-0">
+            Questão 5
+            </h5>
+          </div>
+          <div className="card-body">
+            <Questao4 nome='Francisco Costa Cassemiro Júnior' curso='Sistemas de Informação' cidade='Capistrano - CE'>
+              <Questao2/>
+            </Questao4>
+          </div>              
+      </div>
     </div>
   )
 }
